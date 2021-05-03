1d26592374f6e866c74a9a543e3effea66e91b1a
Z3r0cks <patrick.kaserer@web.de>
2021-05-03 19:15:21 +0200

    ADD: lora presentation code

---
+++ 08_Communication/LoRa_presentation/lora_sender/src/main.cpp
@@ -0,0 +1,53 @@
+
+#include <SPI.h>
+#include <LoRa.h>
+#include <wifi_connection.h>
+
+//define the pins used by the transceiver module
+#define ss 5
+#define rst 14
+#define dio0 2
+
+int counter = 0;
+
+void setup() {
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("LoRa Sender");
+
+  //initialize Wifi connection
+  initWifi();
+  checkWifi();
+
+  //setup LoRa transceiver module
+  LoRa.setPins(ss, rst, dio0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //866E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(866E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+}
+
+void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+  String currenTimeStamp = timeStampRequest();
+  //Send LoRa packet to receiver
+  LoRa.beginPacket();
+  LoRa.println(currenTimeStamp);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(2000);
+}
