820daa8e19272dbd4f16049e8e9421480528c35f
gabriel-rausch <gabriel-rausch@users.noreply.github.com>
2023-03-26 17:38:30 +0200

    add basic blink example

--- 04_Getting_Started/Basics_Hello_World_Blink/src/main.cpp
+++ 04_Getting_Started/Basics_Hello_World_Blink/src/main.cpp
@@ -13,14 +13,14 @@
  * 
  * 
  * #Part 2: Blink LED continuously
- * Connect led to board (e.g. via pin 21). 
+ * Connect led to board (e.g. via pin 13). 
  * Note: make sure to connect led's cathode to ground!
  * Make led shine.
  * Then, write some lines of code to blink the led continuously.
  * 
  * 
  * #Part 3: Touch sensor
- * Connect wire to touch pin (e.g. on Touch0 ~ pin 24)
+ * Connect wire to touch pin (e.g. on Touch0 ~ pin 4)
  * Print touch sensor value to serial monitor
  * 
  * 
@@ -35,9 +35,27 @@
 
 
 void setup() {
+  Serial.begin(115200);
   // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  // turn the LED on (HIGH is the voltage level) and wait 200 ms
+  digitalWrite(13, HIGH);
+  Serial.println("On");
+  delay(200);
+
+  // turn the LED off by making the voltage LOW and wait 200 ms
+  digitalWrite(13, LOW);    
+  Serial.println("Off");
+  delay(200);    
+
+  // if touch sensor on pin 4 is lower than a specific sensitive value
+  if(touchRead(4) < 40) {  
+    // do something, when touch is active
+    Serial.println("Touch Sensor active");
+  }
 }
